cc97e079e2b065bacc29fca4417e8ea2018cd74b
ruturajsawant5 <ruturajsawant5@gmail.com>
2019-01-18 07:18:26 +0530

    array

---
+++ arrayList/arrayList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList a1 = new ArrayList();
+            int[] a2 = new int[5];
+            a1.Add(5);
+            a1.Add(10);
+            a1.Add(15);
+            a1.Add(20);
+            a1.Add(25);
+            a1.Add(5);
+            a1.Add(10);
+            a1.Add(15);
+            a1.Add(20);
+            a1.Add(25);
+
+            bool y = a1.Contains(15);
+
+            a1.CopyTo(0, a2, 0, 4);
+
+            a1.Insert(3, 30);
+
+            int c = a1.Capacity;
+
+            for (int i = 0; i < a1.Count; i++)
+            {
+                System.Console.WriteLine(a1[i]);
+            }
+            System.Console.WriteLine();
+
+            System.Console.WriteLine(y);
+            System.Console.WriteLine();
+
+
+            for (int i = 0; i < a2.Length; i++)
+            {
+                System.Console.WriteLine(a2[i]);
+            }
+            System.Console.WriteLine();
+
+
+            for (int i = 0; i < a1.Count; i++)
+            {
+                System.Console.WriteLine(a1[i]);
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine(c);
+
+
+
+
+            System.Console.ReadKey();
+        }
+    }
---
+++ arrayMethod/arrayMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] arr = new int[] { 5, 8, 3, 9, 1 };
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+
+        int l = arr.GetLength(0);
+        int v = Convert.ToInt32(arr.GetValue(3));
+        int l1 = arr.Length;
+        //Array.Sort(arr);
+        Array.Reverse(arr);
+        Array.Copy(arr, arr1, 5);
+       // arr.CopyTo(0,arr2,0,3);
+
+      
+
+        System.Console.WriteLine(l+"\n");
+        System.Console.WriteLine(v + "\n");
+        System.Console.WriteLine(l1 + "\n");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            System.Console.WriteLine(arr[i]);
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            System.Console.WriteLine(arr1[i]);
+        }
+
+      
+        System.Console.ReadKey();
+
+    }
+}
