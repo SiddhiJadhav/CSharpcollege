69cd5f6f72cc970d536d033eb888dceabeb3c625
ruturajsawant5 <ruturajsawant5@gmail.com>
2019-01-25 07:44:16 +0530

    25/01

---
+++ dllused/dllused/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace dllused
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.display();
+            System.Console.ReadKey();
+        }
+    }
+}
